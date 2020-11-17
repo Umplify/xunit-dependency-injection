bd74b5d98f44bc2303478ab9001992bfcc5f6d61
Arash A. Sabet <Arash-Sabet@users.noreply.github.com>
2020-11-16 21:02:46 -0500

    Added an example to the solution #6

---
+++ examples/Xunit.Microsoft.DependencyInjection.ExampleTests/Fixtures/TestFixture.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Microsoft.DependencyInjection.Abstracts;
+using Xunit.Microsoft.DependencyInjection.ExampleTests.Services;
+
+namespace Xunit.Microsoft.DependencyInjection.ExampleTests.Fixtures
+{
+    public class TestFixture : TestBedFixture
+    {
+        protected override void AddServices(IServiceCollection services, IConfiguration configuration)
+            => services
+                .AddTransient<ICalculator, Calculator>()
+                .Configure<Options>(config => configuration.GetSection("Options").Bind(config));
+
+        protected override string GetConfigurationFile()
+            => "appsettings.json";
+    }
+}
