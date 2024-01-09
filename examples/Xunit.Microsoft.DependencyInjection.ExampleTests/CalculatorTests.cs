7a8ec6d2dcfa22e147adc85cd45a4c5e1b15a4e0
Arash A. Sabet <26050123+Arash-Sabet@users.noreply.github.com>
2024-01-08 19:40:18 -0500

    Some minor refactoring

--- examples/Xunit.Microsoft.DependencyInjection.ExampleTests/CalculatorTests.cs
+++ examples/Xunit.Microsoft.DependencyInjection.ExampleTests/CalculatorTests.cs
@@ -5,30 +5,27 @@ namespace Xunit.Microsoft.DependencyInjection.ExampleTests;
 
 public class CalculatorTests : TestBed<CalculatorFixture>
 {
-    private readonly ICalculator _calculator;
     private readonly Options _options;
 
     public CalculatorTests(ITestOutputHelper testOutputHelper, CalculatorFixture fixture)
-        : base(testOutputHelper, fixture)
-    {
-        _calculator = _fixture.GetService<ICalculator>(_testOutputHelper)!;
-        _options = _fixture.GetService<IOptions<Options>>(_testOutputHelper)!.Value;
-    }
+        : base(testOutputHelper, fixture) => _options = _fixture.GetService<IOptions<Options>>(_testOutputHelper)!.Value;
 
     [Theory]
     [InlineData(1, 2)]
-    public async Task Test1Async(int x, int y)
+    public async Task TestServiceAsync(int x, int y)
     {
-        var calculatedValue = await _calculator.AddAsync(x, y);
+        var calculator = _fixture.GetService<ICalculator>(_testOutputHelper)!;
+        var calculatedValue = await calculator.AddAsync(x, y);
         var expected = _options.Rate * (x + y);
         Assert.True(expected == calculatedValue);
     }
 
     [Theory]
     [InlineData(1, 2)]
-    public async Task Test2Async(int x, int y)
+    public async Task TestScopedServiceAsync(int x, int y)
     {
-        var calculatedValue = await _calculator.AddAsync(x, y);
+        var calculator = _fixture.GetScopedService<ICalculator>(_testOutputHelper)!;
+        var calculatedValue = await calculator.AddAsync(x, y);
         var expected = _options.Rate * (x + y);
         Assert.True(expected == calculatedValue);
     }
