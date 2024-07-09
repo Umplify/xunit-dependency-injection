a332c106a780747df9e1ed1e28ab41101ccc350c
Arash A. Sabet <arash@umplify.com>
2024-07-08 22:27:47 -0400

    #262 Drafted the implementation of support user secrets

---
+++ examples/Xunit.Microsoft.DependencyInjection.ExampleTests/UserSecretTests.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Extensions.Options;
+
+namespace Xunit.Microsoft.DependencyInjection.ExampleTests;
+
+public class UserSecretTests(ITestOutputHelper testOutputHelper, TestProjectFixture fixture) : TestBed<TestProjectFixture>(testOutputHelper, fixture)
+{
+    [Fact]
+    public void TestSecretValues()
+    {
+        var secretValues = _fixture.GetService<IOptions<SecretValues>>(_testOutputHelper)!.Value;
+        Assert.NotEmpty(secretValues?.Secret1 ?? string.Empty);
+        Assert.NotEmpty(secretValues?.Secret1 ?? string.Empty);
+    }
+}
