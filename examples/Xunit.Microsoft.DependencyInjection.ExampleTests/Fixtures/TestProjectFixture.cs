a819ad3ba934629fa7265ae7ae29c504f2cbb831
Arash A. Sabet <arash@umplify.com>
2024-07-09 18:33:06 -0400

    #262 Fixed the user secret configuration issue in the example test project

--- examples/Xunit.Microsoft.DependencyInjection.ExampleTests/Fixtures/TestProjectFixture.cs
+++ examples/Xunit.Microsoft.DependencyInjection.ExampleTests/Fixtures/TestProjectFixture.cs
@@ -8,7 +8,7 @@ public class TestProjectFixture : TestBedFixture
         .AddKeyedTransient<ICarMaker, Porsche>("Porsche")
         .AddKeyedTransient<ICarMaker, Toyota>("Toyota")
         .Configure<Options>(config => configuration?.GetSection("Options").Bind(config))
-        .Configure<SecretValues>(config => configuration?.GetSection(nameof(SecretValues)));
+        .Configure<SecretValues>(config => configuration?.GetSection(nameof(SecretValues)).Bind(config));
 
     protected override ValueTask DisposeAsyncCore()
         => new();
