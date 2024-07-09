e4c9e59faf5f73ae8d4bb13d490e58554eea1416
Arash A. Sabet <arash@umplify.com>
2024-07-09 19:03:25 -0400

    #262 Upgraded the NuGet packages

--- examples/Xunit.Microsoft.DependencyInjection.ExampleTests/UserSecretTests.cs
+++ examples/Xunit.Microsoft.DependencyInjection.ExampleTests/UserSecretTests.cs
@@ -8,6 +8,14 @@ public class UserSecretTests(ITestOutputHelper testOutputHelper, TestProjectFixt
     [Fact]
     public void TestSecretValues()
     {
+        /*
+         * TODO: Create a user secret entry like the following payload in user secrets and remove the same from appsettings.json file:
+         * 
+         * "SecretValues": {
+         *   "Secret1": "secret1value",
+         *   "Secret2": "secret2value"
+         * }
+         */
         var secretValues = _fixture.GetService<IOptions<SecretValues>>(_testOutputHelper)!.Value;
         Assert.NotEmpty(secretValues?.Secret1 ?? string.Empty);
         Assert.NotEmpty(secretValues?.Secret1 ?? string.Empty);
