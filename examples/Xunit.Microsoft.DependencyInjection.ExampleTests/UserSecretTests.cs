b4dc253ec0ff9eb159b39b465a6850e4f769377f
Christian Dörnen <mail@christiandoernen.de>
2025-12-16 18:33:04 +0100

    Programatically set (random) user secrets using dotnet command. Then assert the secrets are available in the application via configuration.

--- examples/Xunit.Microsoft.DependencyInjection.ExampleTests/UserSecretTests.cs
+++ examples/Xunit.Microsoft.DependencyInjection.ExampleTests/UserSecretTests.cs
@@ -1,23 +1,45 @@
 ﻿
+using System.Diagnostics;
 using Microsoft.Extensions.Options;
 
 namespace Xunit.Microsoft.DependencyInjection.ExampleTests;
 
-public class UserSecretTests(ITestOutputHelper testOutputHelper, TestProjectFixture fixture) : TestBed<TestProjectFixture>(testOutputHelper, fixture)
+public class UserSecretTests : TestBed<TestProjectFixture>
 {
+    private static readonly Guid Guid = Guid.NewGuid(); // Ensure unique user secrets per test run
+
+    private readonly string _secret1Value = $"secret1-{Guid}";
+    private readonly string _secret2Value = $"secret2-{Guid}";
+    
+    public UserSecretTests(ITestOutputHelper testOutputHelper, TestProjectFixture fixture) : base(testOutputHelper, fixture)
+    {
+        SetSecret(nameof(SecretValues.Secret1), _secret1Value);
+        SetSecret(nameof(SecretValues.Secret2), _secret2Value);
+    }
+
     [Fact]
     public void TestSecretValues()
     {
-        /*
-         * TODO: Create a user secret entry like the following payload in user secrets and remove the same from appsettings.json file:
-         * 
-         * "SecretValues": {
-         *   "Secret1": "secret1value",
-         *   "Secret2": "secret2value"
-         * }
-         */
         var secretValues = _fixture.GetService<IOptions<SecretValues>>(_testOutputHelper)!.Value;
-        Assert.NotEmpty(secretValues?.Secret1 ?? string.Empty);
-        Assert.NotEmpty(secretValues?.Secret1 ?? string.Empty);
+        Assert.Equal(secretValues.Secret1, _secret1Value);
+        Assert.Equal(secretValues.Secret2, _secret2Value);
+    }
+
+    private void SetSecret(string secretName, string secretValue)
+    {
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            Arguments = $"user-secrets set {nameof(SecretValues)}:{secretName} {secretValue}",
+            WorkingDirectory = Path.Combine(Environment.CurrentDirectory, "..", "..", "..")
+        };
+        var proc = Process.Start(startInfo);
+        ArgumentNullException.ThrowIfNull(proc);
+        
+        proc.WaitForExit();
+        if (proc.ExitCode != 0)
+        {
+            throw new Exception("Failed to set user secret");
+        }
     }
 }
